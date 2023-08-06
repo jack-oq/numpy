e57b81de51b2b5b826d758001cfc5a9adbc15c28
jack-oq <106361345+jack-oq@users.noreply.github.com>
2023-08-06 17:50:51 -0500

    Add files via upload

---
+++ Arrays1.py
@@ -0,0 +1,37 @@
+## Numpy Exercise
+import numpy as np 
+
+## Step 1: Create a 4x3 array of all 2s
+print("-----------------------------------------------   STEP ONE   -----------------------------------------------")
+
+print()
+
+## Step 2: Create a 3x4 array with a range from 0 to 110 where each number increases by 10
+print("-----------------------------------------------   STEP TWO   -----------------------------------------------")
+
+print() 
+
+## Step 3: Change the layout of the above array to be 4x3, store it in a new array
+print("-----------------------------------------------   STEP THREE   -----------------------------------------------")
+
+print()
+
+## Step 4: Multiply every elemnt of the above array by 3 and store the new values in a different array
+print("-----------------------------------------------   STEP FOUR   -----------------------------------------------")
+
+print()
+
+## Step 5: Multiply your array from step one by your array from step 2
+print("-----------------------------------------------   STEP FIVE   -----------------------------------------------")
+
+## This errored out... why?
+print()
+
+## Step 6: Comment out your code from Step 5 and then multiply your array from step 1 by your array from step 3
+print("-----------------------------------------------   STEP SIX   -----------------------------------------------")
+
+## this worked! why?
+print()
+
+
+
---
+++ Arrays2.py
@@ -0,0 +1,54 @@
+## Numpy Exercise 2
+import numpy as np
+
+## This array documnets the last 5 sales for each of Superstore's four cash registers. 
+salesArray = np.array([[150.68,207.99,107.88,58.99,60.59],[20.89,98.99,258.62,19.76,101.59],[70.66,190.10,134.54,200.14,15.59],[10.52,201.59,140.55,13.99,45.98]])
+
+## Step 1: Print the total sales for the store.
+print("-----------------------------------------------   STEP ONE   -----------------------------------------------")
+
+print()
+
+## Step 2: What was Superstore's smallest and largest sale? Print them.
+print("-----------------------------------------------   STEP TWO   -----------------------------------------------")
+
+print()
+
+## Step 3: Print an array that will show which sales are greater than or equal to $100.
+print("-----------------------------------------------   STEP THREE  -----------------------------------------------")
+
+print()
+
+## Step 4: Print the total sales for each register.
+print("-----------------------------------------------   STEP FOUR   -----------------------------------------------")
+
+print()
+
+## Step 5: Superstore is a cashless store and needs to calculate their owed credit card fees. Each sale is subject to a 2% credit card fee.
+#           Using the salesArray, create a new array that stores the 2% fee for each sale and register. Print the array and then print the total fees.
+print("-----------------------------------------------   STEP FIVE  -----------------------------------------------")
+
+print()
+
+## Step 6: Using your fee array and salesArray, calculate how much profit Superstore made for each sale after paying credit card fees. Store this in a new array and print it.
+print("-----------------------------------------------   STEP SIX  -----------------------------------------------")
+
+print()
+
+## Step 7: Print the sales only for the second and forth cash register
+print("-----------------------------------------------   STEP SEVEN  -----------------------------------------------")
+
+print()
+
+## Step 8: Superstore has added a 5th cash register who's data is stored in the array newRegister. Add the new register to the original array. Print the updated salesArray.
+print("-----------------------------------------------   STEP EIGHT  -----------------------------------------------")
+newRegister = np.array([17.89,13.59,107.89,176.88,56.78])
+
+print()
+
+## Step 9: Register #3 had an error and recorded it's fourth sale ($200.14) incorrectly. The sale should have been $20.14. Update the array to correct this error.
+#           Print the array before and after the update to see the change.
+print("-----------------------------------------------   STEP NINE  -----------------------------------------------")
+
+print()
+
